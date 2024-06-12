1e0a181b22243d584ead5ae767f47129798b0ea5
Satria Manggala Jati <39191313+satriamjati@users.noreply.github.com>
2024-06-12 18:00:23 +0700

    Set custom-image-loader.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ module.exports = {
   reactStrictMode: true,
   trailingSlash: true,
   basePath: '/NextJsDemoPage',
+  images:{
+        loader:"custom",
+        loaderFile:"/NextJsDemoPage/custom-image-loader.js",
   exportPathMap: function () {
       return {
           '/': { page: '/' }
